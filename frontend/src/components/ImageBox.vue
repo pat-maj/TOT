89d7d59e6bb8dc7be0b5dee8f70c071deb62a76c
pat-maj <patrykk.majcherr@gmail.com>
2024-04-24 00:53:21 +0100

    Add Welcome Page, Bootstrap, Tornament Structure Page, and ImageBox component as well as delete examples

---
+++ frontend/src/components/ImageBox.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+    data() {
+        return{
+            
+        }
+    },
+    props: {
+        image_URL: String,
+        title: String,
+        description: String
+    }
+}
+</script>
+
+<template>
+    <div class="border max-width-250 rounded mb-4">
+        <img :src="image_URL" class="max-width-250"/>
+        <div class="text-center">
+            <h3>{{ title }}</h3>
+            <p>{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.max-width-250{
+    max-width: 250px;
+}
+</style>
---
+++ frontend/src/views/TournamentStructureView.vue
@@ -0,0 +1,22 @@
+<script>
+import ImageBox from '../components/ImageBox.vue'
+export default {
+    components: {
+        ImageBox
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Choose Tournament Structure</h1>
+    </div>
+    <div class="d-flex flex-row flex-wrap justify-content-around">
+        <ImageBox image_URL="src/assets/image.png" title="Test" description="Nice short description just to ensure everything's great"/>
+        <ImageBox image_URL="src/assets/image.png" title="Test" description="Nice short description just to ensure everything's great"/>
+        <ImageBox image_URL="src/assets/image.png" title="Test" description="Nice short description just to ensure everything's great"/>
+    </div>
+</template>
+
+<style>
+</style>
